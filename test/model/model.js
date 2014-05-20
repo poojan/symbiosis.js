717efce9f2609c944d2c08e7aa5d595f37805640
Kenneth Lynne <kenneth.lynne@gmail.com>
2014-05-20 20:27:39 +0200

    scaffold model

---
+++ test/model/model.js
@@ -0,0 +1,17 @@
+"use strict";
+
+describe('ORM', function () {
+  var ORM = window.ORM;
+
+  describe('Model', function () {
+
+    describe('define', function () {
+      it('should return a model', function() {
+        var Person = ORM.Model.Define('Person', {});
+
+      });
+    });
+
+  });
+
+});
--- test/orm.js
+++ test/orm.js
@@ -1,10 +1,5 @@
+"use strict";
+
 describe('ORM', function () {
-
-  describe('define', function () {
-    it('should return a model', function() {
-      var Person = ORM.Define('Person', {});
-
-    });
-  });
 
 });
