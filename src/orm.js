b81bff3a31edbe371d819682b86f69d4c0c5233c
Kenneth Lynne <kenneth.lynne@gmail.com>
2014-05-20 21:03:02 +0200

    implement generic loader

--- src/orm.js
+++ src/orm.js
@@ -1,6 +1,15 @@
-;(function (window, undefined) {
-  var _ORM = {};
+;(function (name, definition) {
+  if (typeof module !== 'undefined') {
+    module.exports = definition();
+  } else if (typeof define === 'function' && typeof define.amd === 'object') {
+    define(definition);
+  } else {
+    this[name] = definition();
+  }
+})('ORM', function () {
+  'use strict';
 
+  var ORM = {};
 
-  window.ORM = _ORM;
-})(window);
+  return ORM;
+});
