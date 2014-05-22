abc380952d4dcc231e75cbeda287abcad680c8cd
James C Russell <james@101ideas.cz>
2014-05-22 08:57:16 +0200

    Traceur task for gulp added.

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,9 @@
 var gulp = require('gulp');
+
+var traceur = require('gulp-traceur');
+
+gulp.task('default', function () {
+  return gulp.src('test.js')
+             .pipe(traceur({sourceMap: true}))
+             .pipe(gulp.dest('dist'));
+});
