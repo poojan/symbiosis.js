2ce4edc7c67be97da5dbf5e22107e47b02bd7a94
Kenneth Lynne <kenneth.lynne@gmail.com>
2014-05-26 01:09:15 +0200

    support building to both cjs and amd

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,39 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+  traceur = require('gulp-traceur'),
 
-var traceur = require('gulp-traceur');
+  path = {
+    src: './src/**/*.js',
+    pkg: './package.json'
+  };
 
-gulp.task('default', function () {
-  return gulp.src('test.js')
-             .pipe(traceur({sourceMap: true}))
-             .pipe(gulp.dest('dist'));
+
+gulp.task('build_source_amd', function () {
+  gulp.src(path.src)
+    .pipe(traceur({
+      modules: 'amd',
+      types: true,
+      annotations: true,
+      typeAssertions: true,
+      typeAssertionModule: 'assert'
+    }))
+    .pipe(gulp.dest('dist/amd'));
+});
+
+gulp.task('build_source_cjs', function () {
+  gulp.src(path.src)
+    .pipe(traceur({
+      modules: 'commonjs',
+      types: true,
+      annotations: true,
+      typeAssertions: true,
+      typeAssertionModule: 'assert'
+    }))
+    .pipe(gulp.dest('dist/cjs'));
+});
+
+gulp.task('build_dist', ['build_source_cjs', 'build_source_amd']);
+gulp.task('build', ['build_dist']);
+
+gulp.task('watch', function () {
+  gulp.watch(path.src, ['build']);
 });
