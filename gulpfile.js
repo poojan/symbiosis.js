589d2902ce724c47b602ff8d4299f02d055aef33
Poojan Shrestha <poojans@gmail.com>
2014-05-26 05:09:55 +0545

    Added gulp clean, build and watch tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,24 @@
-var gulp = require('gulp');
+'use strict';
 
+var gulp = require('gulp');
+var clean = require('gulp-clean');
 var traceur = require('gulp-traceur');
 
-gulp.task('default', function () {
-  return gulp.src('test.js')
-             .pipe(traceur({sourceMap: true}))
-             .pipe(gulp.dest('dist'));
+gulp.task('clean', function () {
+  gulp.src('dist/**/*', { read: false })
+    .pipe(clean({ force: true }));
 });
+
+gulp.task('traceur', function () {
+  gulp.src('src/**/*.js')
+    .pipe(traceur())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build', ['clean', 'traceur']);
+
+gulp.task('watch', ['build'], function () {
+  gulp.watch('src/**/*.js', ['build']);
+});
+
+gulp.task('default', ['build']);
