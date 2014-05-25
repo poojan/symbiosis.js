b033c4eaef395ce98027220bc8214c243711fd33
Kenneth Lynne <kenneth.lynne@gmail.com>
2014-05-26 00:18:03 +0200

    remove redundant test

--- test/unit/ormSpec.js
+++ test/unit/ormSpec.js
@@ -4,14 +4,4 @@ describe('ORM', function () {
   it('should be defined', function () {
     expect(ORM).toBeDefined();
   });
-
-  describe('Injector', function () {
-    describe('register', function () {
-      it('should register a new factory', function () {
-        //TODO: Nah.. Needs better planning
-        ORM.Core.Injector.register('name', 'something');
-        ORM.Core.Injector.get('name').toBe('something');
-      });
-    });
-  });
 });
