9def96d0802daf6044c41d81d2397475cb577451
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-05-30 17:17:31 +0300

    base spec added

---
+++ test/spec/modelSpec.js
@@ -0,0 +1,27 @@
+describe('model specs', function () {
+  describe('when model is persisted', function () {
+    beforeEach(function () {
+      // define model
+    });
+
+    beforeEach(function () {
+      // initialize model with data
+    });
+
+    beforeEach(function () {
+      // save the model
+    });
+
+    beforeEach(function () {
+      // get by id
+    });
+
+    it('should be persisted', function () {
+      // verify that model have proper id after save
+    });
+
+    it('should be fetched', function () {
+      // verify that model restored and all properties initialized
+    });
+  })
+});
